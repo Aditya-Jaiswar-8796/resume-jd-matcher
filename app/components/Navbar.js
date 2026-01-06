c2dc333d59d91aa3f4fcd931cbf64de891730253
Aditya-Jaiswar-8796 <adityajaiswar8796@gmail.com>
2026-01-06 21:50:01 +0530

    navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-300/50 border-b-2 rounded-b-lg shadow-[1px_1px_.2rem_grey] flex justify-between border-gray-900/50 w-full h-12 p-2 items-center'>
+      <div className="flex justify-center items-center gap-2">
+        <img src="/logo.gif" alt=""  width={30}/>
+        <h1 className='text-xl text-shadow-white font-bold'>Resumer</h1>
+      </div>
+      <div className="text-sm text-shadow-white font-semibold">
+        We are here to help you
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
