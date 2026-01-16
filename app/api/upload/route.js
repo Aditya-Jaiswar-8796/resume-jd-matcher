bf819de1cc0848e016d1d5a3349b9b5a04d694ab
Aditya-Jaiswar-8796 <adityajaiswar8796@gmail.com>
2026-01-16 13:38:52 +0530

    experience calculated

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -7,66 +7,83 @@ export async function POST(req) {
   const formData = await req.formData();
   const files = formData.getAll('file') || [];
   const results = [];
-const parser = new SmartParser();
+  const parser = new SmartParser();
   for (const file of files) {
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     if (file.name.split('.')[file.name.split('.').length - 1] === "pdf") {
       const data = await parser.parse(buffer);
-      
+
       results.push({
         name: file.name,
         text: data.text,
 
       });
-      
+
     } else {
       const data = await mammoth.extractRawText(buffer);
-      
+
       results.push({
         name: file.name,
         text: data.value,
       });
     }
   }
-const SECTION_MAP = {
-  summary: /^(summary|profile)$/i,
-  experience: /^(experience|work experience|employment)$/i,
-  skills: /^(skills|technical skills|technologies)$/i,
-  education: /^(education|academic)$/i,
-  projects: /^(projects|open source)$/i
-};
+  const SECTION_MAP = {
+    summary: /^(summary|profile)$/i,
+    experience: /^(experience|work experience|employment)$/i,
+    skills: /^(skills|technical skills|technologies)$/i,
+    education: /^(education|academic)$/i,
+    projects: /^(projects|open source)$/i
+  };
   for (const result of results) {
-   result.rawText = result.text
-  .toLowerCase()
-  .replace(/https?:\/\/\S+/g, "")            
-  .replace(/\S+@\S+\.\S+/g, "")               
-  .replace(/[^\w\s]/g, "")
-  .replace(/(\+?\d{10,})/g, "")                         
-  .split("\n").filter(value => Boolean(value));
+    result.rawText = result.text
+      .toLowerCase()
+      .replace(/https?:\/\/\S+/g, "")
+      .replace(/\S+@\S+\.\S+/g, "")
+      .replace(/[^\w\s]/g, "")
+      .replace(/(\+?\d{10,})/g, "")
+      .split("\n").filter(value => Boolean(value));
 
-  result.sections = {};
-  let currentSection = null;
-  for (const line of result.rawText) {
-    if (typeof line !== "string") return;
-    let isSectionHeader = false;
-    for (const [key, value] of Object.entries(SECTION_MAP)) {
-      if(value.test(line.trim())) {
-        currentSection = key;
-        if(!result.sections[currentSection]){
-        result.sections[currentSection] = [];
+    result.sections = {};
+    let currentSection = null;
+    for (const line of result.rawText) {
+      if (typeof line !== "string") return;
+      let isSectionHeader = false;
+      for (const [key, value] of Object.entries(SECTION_MAP)) {
+        if (value.test(line.trim())) {
+          currentSection = key;
+          if (!result.sections[currentSection]) {
+            result.sections[currentSection] = [];
+          }
+          isSectionHeader = true;
+          break;
+        }
       }
-       isSectionHeader = true;
-       break;
+      if (!isSectionHeader && currentSection) {
+        if (SECTION_MAP.experience.test(currentSection)) {
+          let newLine = line.replace(/[^\d{4}\s+\-*\d{4}]/gi, "").trim();
+          if (newLine !== "") {
+            const n1Match = newLine.match(/^\d{4}/);
+            const n2Match = newLine.match(/\d{4}$/);
+            
+            if (n1Match && n2Match) {
+              const n1 = Number(n1Match[0]);
+              const n2 = Number(n2Match[0]);
+              newLine = n2 - n1;
+              console.log("Experience Line:", newLine);
+              result.sections[currentSection].push(newLine);
+            }
+          }
+        }
+        else {
+          result.sections[currentSection].push(line);
+        }
       }
     }
-    if(!isSectionHeader && currentSection) {
-      result.sections[currentSection].push(line);
-    }
-  }
- 
-  result.text = result.text  
-  .replace(/\s+/g, " ");   
+    result.sections.experience = result.sections.experience.reduce((acc, curr) => acc+curr);
+    result.text = result.text
+      .replace(/\s+/g, " ");
   }
 
 
